3c79ca346e9e4f6161f81c59117668e0adcc8093
kieron <lewis_the_cat@hotmail.com>
2014-08-08 05:33:12 +0100

    2nd go

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var logfmt = require('logfmt');
 var app = express();
+app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
     res.send('hello');
