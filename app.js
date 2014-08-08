84526a571aad2c9c208ce6385e280055386a31aa
kieron <lewis_the_cat@hotmail.com>
2014-08-08 05:42:00 +0100

    4

--- app.js
+++ app.js
@@ -8,4 +8,6 @@ app.get('/', function(req, res) {
 });
 app.use(function(err, req, res, next){ if (req.xhr) { res.send(500, 'Something went wrong!'); } else { next(err); } });
 
+console.log('starting');
 app.listen(8080);
+console.log('listening on 8080');
