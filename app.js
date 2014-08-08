8dbf5753b6fae75c77912360e6e5e5b56d1a81b0
kieron <lewis_the_cat@hotmail.com>
2014-08-08 05:39:18 +0100

    3rd

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
     res.send('hello');
 });
+app.use(function(err, req, res, next){ if (req.xhr) { res.send(500, 'Something went wrong!'); } else { next(err); } });
 
 app.listen(8080);
