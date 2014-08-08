a95a1a31ea9eab7b3bdcb7dfc418a5cbe1fd94e4
kieron <lewis_the_cat@hotmail.com>
2014-08-08 06:12:13 +0100

    server change

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var express = require('express');
 var logfmt = require('logfmt');
 var app = express();
 app.use(logfmt.requestLogger());
-app.use('/scripts', gzippo.staticGzip('' + __dirname + '/../dist/scripts'));
-app.use('/images', gzippo.staticGzip('' + __dirname + '/../dist/images'));
-app.use('/styles', gzippo.staticGzip('' + __dirname + '/../dist/styles'));
-app.use('/views', gzippo.staticGzip('' + __dirname + '/../dist/views'));
+app.use('/scripts', gzippo.staticGzip('' + __dirname + '/dist/scripts'));
+app.use('/images', gzippo.staticGzip('' + __dirname + '/dist/images'));
+app.use('/styles', gzippo.staticGzip('' + __dirname + '/dist/styles'));
+app.use('/views', gzippo.staticGzip('' + __dirname + '/dist/views'));
 app.all('/*', function(req, res, next) {
-    res.sendfile('index.html', {root: __dirname + '/../dist'});
+    res.sendfile('index.html', {root: __dirname + '/dist'});
 });
 app.listen(process.env.PORT || 5000);
