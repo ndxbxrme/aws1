f6683f72f6f9fcbe6fa64d17758dba5d569274af
kieron <lewis_the_cat@hotmail.com>
2014-08-08 05:54:09 +0100

    6

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', function(req, res) {
 app.use(function(err, req, res, next){ if (req.xhr) { res.send(500, 'Something went wrong!'); } else { next(err); } });
 
 console.log('starting');
-app.listen(80);
-console.log('listening on 80');
+app.listen(5000);
+console.log('listening on 5000');
