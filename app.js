51e0b89102f0a40a749303029b78baa42cfdce8b
kieron <lewis_the_cat@hotmail.com>
2014-08-08 06:58:38 +0100

    another port change2

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.use('/views', gzippo.staticGzip('' + __dirname + '/dist/views'));
 app.all('/*', function(req, res, next) {
     res.sendfile('index.html', {root: __dirname + '/dist'});
 });
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 5000);
